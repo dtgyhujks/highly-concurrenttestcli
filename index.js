87f87c419d73383ce2b50489c646f910fb1eeb68
dtgyhujks <gs1moufid14@hotmail.com>
2024-05-17 23:03:44 +0700

    update aptos

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function searchInsert(nums, target) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] === target) return mid;
-    if (nums[mid] < target) left = mid + 1;
-    else right = mid - 1;
+function minMeetingRooms(intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+  const minHeap = new MinHeap();
+  for (const interval of intervals) {
+    if (minHeap.size() > 0 && minHeap.peek() <= interval[0]) {
+      minHeap.pop();
+    }
+    minHeap.push(interval[1]);
   }
-  return left;
+  return minHeap.size();
 }
